f391b0deff925116e3cd9f01d81461aeae364e69
tomaszrazmuk <tomaszrazmuk@gmail.com>
2018-06-18 18:24:31 +0200

    commit2

--- js/app.js
+++ js/app.js
@@ -1,22 +1,49 @@
-var i = 0;
-var images =[];
-var time = 3000;
+var sliderImages = document.querySelectorAll(".slide"),
+    arrowLeft = document.querySelector("#arrow-left"),
+    arrowRight = document.querySelector("#arrow-right"),
+    current = 0;
 
-images[0] = 'slider1.jpg';
-images[1] = 'slider2.jpg';
-images[2] = 'slider3.jpg';
-images[3] = 'slider4.jpg';
-
-function changeImg(){
-    document.slide.src = images[i];
-
-    if( i<images.length -1 ){
-
-        i++;
-
-    } else {
-
-        i = 0;
+// Clear all images
+function reset() {
+    for (let i = 0; i < sliderImages.length; i++) {
+        sliderImages[i].style.display = "none";
     }
-    setTimeout("changeImg()", time);
 }
+
+// Init slider
+function startSlide() {
+    reset();
+    sliderImages[0].style.display = "block";
+}
+
+// Show prev
+function slideLeft() {
+    reset();
+    sliderImages[current - 1].style.display = "block";
+    current--;
+}
+
+// Show next
+function slideRight() {
+    reset();
+    sliderImages[current + 1].style.display = "block";
+    current++;
+}
+
+// Left arrow click
+arrowLeft.addEventListener("click", function() {
+    if (current === 0) {
+        current = sliderImages.length;
+    }
+    slideLeft();
+});
+
+// Right arrow click
+arrowRight.addEventListener("click", function() {
+    if (current === sliderImages.length - 1) {
+        current = -1;
+    }
+    slideRight();
+});
+
+startSlide();
